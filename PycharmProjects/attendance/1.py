7085b4bdb35e33e0ed4ccf3c7ee1f51016455a5f
hishamgit <mohammedhishamk81@gmail.com>
2023-09-28 01:14:52 +0530

    face capture,frame resizing,face comparison

--- PycharmProjects/attendance/1.py
+++ PycharmProjects/attendance/1.py
@@ -28,8 +28,26 @@ face_locations = []
 face_encodings = []
 face_names = []
 s = True
+
 now = datetime.now()
 current_date = now.strftime("%Y/%m/%d")
 
 f = open(current_date + '.csv', 'w+', newline='')
-lnwriter=csv.writer(f)
+lnwriter = csv.writer(f)
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+    if s:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
+            name = ""
+            face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
+            best_match_index=np.argmin(face_distance)
+            if matches[best_match_index]:
+                name=known_face_names[best_match_index]
+            face_names.append(name)
+            
