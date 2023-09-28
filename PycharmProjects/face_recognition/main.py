647d66ab2269f9f8adfdda8b682fe9de542a3cf8
hishamgit <mohammedhishamk81@gmail.com>
2023-09-29 01:59:59 +0530

    update csv file,finishing

--- PycharmProjects/face_recognition/main.py
+++ PycharmProjects/face_recognition/main.py
@@ -1,4 +1,3 @@
-import pyforest
 import face_recognition
 import cv2
 import numpy as np
@@ -13,7 +12,7 @@ abu_encoding = face_recognition.face_encodings(abu_image)[0]
 manu_image = face_recognition.load_image_file("photos/manu.jpg")
 manu_encoding = face_recognition.face_encodings(manu_image)[0]
 
-rocky_image = face_recognition.load_image_file("photos/rocky.jpg")
+rocky_image = face_recognition.load_image_file("photos/rockey.jpeg")
 rocky_encoding = face_recognition.face_encodings(rocky_image)[0]
 
 damu_image = face_recognition.load_image_file("photos/damu.jpg")
@@ -30,7 +29,7 @@ face_names = []
 s = True
 
 now = datetime.now()
-current_date = now.strftime("%Y/%m/%d")
+current_date = now.strftime("%Y-%m-%d")
 
 f = open(current_date + '.csv', 'w+', newline='')
 lnwriter = csv.writer(f)
@@ -38,6 +37,7 @@ while True:
     _, frame = video_capture.read()
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = small_frame[:, :, ::-1]
+    rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
     if s:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -48,5 +48,19 @@ while True:
             face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
             best_match_index = np.argmin(face_distance)
             if matches[best_match_index]:
-                name =known_face_names[best_match_index]
+                name = known_face_names[best_match_index]
+
             face_names.append(name)
+            if name in known_face_names:
+                if name in students:
+                    students.remove(name)  # to prevent multiple entries
+                    print(students)
+                    lnwriter.writerow([name, now.strftime("%H-%M-%S")])
+
+    cv2.imshow("attendance system", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
